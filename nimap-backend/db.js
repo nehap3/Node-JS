b2e95f82153eeb06d31ce93fcdda242a50ce797d
Neha <neha0307work@gmail.com>
2026-03-01 23:18:43 +0530

    first commit

--- nimap-backend/db.js
+++ nimap-backend/db.js
@@ -1,22 +1,18 @@
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'nimap_test',
-  port: process.env.DB_PORT || 3306,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: 'localhost',
+  user: 'root',
+  password: 'root',   
+  database: 'nimap_test',
+  port: 3306    
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error("DB Connection Failed ❌", err.message);
+    console.log("DB Connection Failed", err);
   } else {
-    console.log("MySQL Connected (Cloud) ✅");
+    console.log("MySQL Connected");
   }
 });
 
