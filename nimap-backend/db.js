0ff5978f4133b9e93b570f2c411508c72788af17
Neha <neha0307work@gmail.com>
2026-03-01 22:37:29 +0530

    first commit

--- nimap-backend/db.js
+++ nimap-backend/db.js
@@ -1,15 +1,22 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'nimap_test'
+  host: 'mysql-instance.aivencloud.com',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'nimap_test',
+  port: 12345,   // VERY IMPORTANT (Aiven gives custom port)
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+  if (err) {
+    console.log("DB Connection Failed ❌", err);
+  } else {
+    console.log("MySQL Connected (Cloud) ✅");
+  }
 });
 
 module.exports = db;
